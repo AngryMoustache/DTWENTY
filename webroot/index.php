667853a22b242b785d58e1663a6d0a82bb955d15
AngryMoustache <mrclotion@hotmail.com>
2018-11-09 17:55:21 +0100

    Configure test

--- webroot/index.php
+++ webroot/index.php
@@ -4,4 +4,12 @@
 
     // Database connecting
 
-    //
+    // Load stuff
+    include_once('../Core/Configure/Configure.php');
+
+    // $config = new Configure();
+    // $config->write('test', array('test', 'test'));
+    // var_dump($config->read('test'));
+
+    Configure::write('test', array('test'));
+    Configure::read('test');
