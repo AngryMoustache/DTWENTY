b217483b1c969c20e21ac036b5123fa288473c9f
AngryMoustache <mrclotion@hotmail.com>
2018-11-17 15:56:50 +0100

    Routes update

--- Config/routes.php
+++ Config/routes.php
@@ -13,50 +13,11 @@
 *       )
 *   );
 */
+
 Route::add(
     array(
         'path' => '/',
         'controller' => 'HomeController',
         'action' => 'home'
-    )
-);
-
-Route::add(
-    array(
-        'path' => '/users',
-        'controller' => 'UserController',
-        'action' => 'overview'
-    )
-);
-
-Route::add(
-    array(
-        'path' => '/users/create',
-        'controller' => 'UserController',
-        'action' => 'create'
-    )
-);
-
-Route::add(
-    array(
-        'path' => '/users/:id',
-        'controller' => 'UserController',
-        'action' => 'single'
-    )
-);
-
-Route::add(
-    array(
-        'path' => '/users/:id/delete',
-        'controller' => 'UserController',
-        'action' => 'delete'
-    )
-);
-
-Route::add(
-    array(
-        'path' => '/tags',
-        'controller' => 'TagController',
-        'action' => 'overview'
     )
 );
